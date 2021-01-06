aada85452e8d82985db581b8c67fe11909273f37
Jonathan Knowles <jonathan.knowles@iohk.io>
2021-01-06 08:54:18 +0000

    Add function `padCoalesce`.

--- lib/numeric/test/unit/Cardano/Numeric/UtilSpec.hs
+++ lib/numeric/test/unit/Cardano/Numeric/UtilSpec.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TypeApplications #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Cardano.Numeric.UtilSpec where
@@ -9,11 +10,13 @@ import Cardano.Numeric.PositiveNatural
 import Cardano.Numeric.PositiveNatural.Gen
     ( genPositiveNaturalAny, shrinkPositiveNaturalAny )
 import Cardano.Numeric.Util
-    ( partitionNaturalMaybe )
+    ( padCoalesce, partitionNaturalMaybe )
 import Data.List.NonEmpty
     ( NonEmpty (..) )
 import Data.Maybe
     ( catMaybes )
+import Data.Monoid
+    ( Sum (..) )
 import Data.Ratio
     ( (%) )
 import Numeric.Natural
@@ -39,6 +42,15 @@ import qualified Data.List.NonEmpty as NE
 spec :: Spec
 spec = do
 
+    describe "padCoalesce" $ do
+
+        it "prop_padCoalesce_length" $
+            property $ prop_padCoalesce_length @(Sum Int)
+        it "prop_padCoalesce_sort" $
+            property $ prop_padCoalesce_sort @(Sum Int)
+        it "prop_padCoalesce_sum" $
+            property $ prop_padCoalesce_sum @(Sum Int)
+
     describe "partitionNaturalMaybe" $ do
 
         it "prop_partitionNaturalMaybe_length" $
@@ -47,6 +59,27 @@ spec = do
             property prop_partitionNaturalMaybe_sum
         it "prop_partitionNaturalMaybe_fair" $
             withMaxSuccess 1000 $ checkCoverage prop_partitionNaturalMaybe_fair
+
+--------------------------------------------------------------------------------
+-- Coalescing values
+--------------------------------------------------------------------------------
+
+prop_padCoalesce_length
+    :: (Monoid a, Ord a, Show a) => NonEmpty a -> NonEmpty () -> Property
+prop_padCoalesce_length source target =
+    NE.length (padCoalesce source target) === NE.length target
+
+prop_padCoalesce_sort
+    :: (Monoid a, Ord a, Show a) => NonEmpty a -> NonEmpty () -> Property
+prop_padCoalesce_sort source target =
+    NE.sort result === result
+  where
+    result = padCoalesce source target
+
+prop_padCoalesce_sum
+    :: (Monoid a, Ord a, Show a) => NonEmpty a -> NonEmpty () -> Property
+prop_padCoalesce_sum source target =
+    F.fold source === F.fold (padCoalesce source target)
 
 --------------------------------------------------------------------------------
 -- Partitioning natural numbers

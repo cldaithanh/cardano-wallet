d1b97f8ecf28e9f58cd2c02aca8072f6e23088cf
Jonathan Knowles <jonathan.knowles@iohk.io>
2021-01-06 07:23:11 +0000

    Add type `RoundingDirection`.
    Add type `RoundingDirection`, which represents a rounding direction,
    either `RoundUp`, or `RoundDown`.

---
+++ lib/numeric/src/Cardano/Numeric/Rounding.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE LambdaCase #-}
+
+-- | Provides types and functions relating to rounding of fractional numbers.
+--
+module Cardano.Numeric.Rounding
+    ( RoundingDirection (..)
+    , round
+    ) where
+
+import Prelude hiding
+    ( round )
+
+-- | Indicates a rounding direction to be used when converting from a
+--   fractional value to an integral value.
+--
+-- See 'round'.
+--
+data RoundingDirection
+    = RoundUp
+      -- ^ Round up to the nearest integral value.
+    | RoundDown
+      -- ^ Round down to the nearest integral value.
+    deriving (Eq, Show)
+
+-- | Use the given rounding direction to round the given fractional value,
+--   producing an integral result.
+--
+round :: (RealFrac a, Integral b) => RoundingDirection -> a -> b
+round = \case
+    RoundUp -> ceiling
+    RoundDown -> floor

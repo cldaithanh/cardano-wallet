b31d9acf1a0d4ba8d1ac25799f7d7d1de267cc01
Jonathan Knowles <jonathan.knowles@iohk.io>
2022-04-10 02:22:54 +0000

    Add coverage checks for `Difference` laws.

--- lib/core/src/Algebra/Difference.hs
+++ lib/core/src/Algebra/Difference.hs
@@ -20,7 +20,7 @@ import Data.Set
 import Numeric.Natural
     ( Natural )
 import Test.QuickCheck
-    ( Arbitrary, property )
+    ( Arbitrary, Property, checkCoverage, cover, property )
 import Test.QuickCheck.Classes
     ( Laws (..) )
 
@@ -32,6 +32,13 @@ import qualified Data.Set as Set
 
 class Difference a where
     difference :: a -> a -> a
+
+--------------------------------------------------------------------------------
+-- Laws
+--------------------------------------------------------------------------------
+
+-- TODO: When testing laws, also test inherited laws for superclasses, because
+-- we can get better coverage.
 
 --------------------------------------------------------------------------------
 -- Laws: Difference Eq Monoid
@@ -152,12 +159,19 @@ laws_Difference_Eq_Monoid
     -> Laws
 laws_Difference_Eq_Monoid _ = Laws "Difference Eq Monoid"
     [ ( "Difference Eq Monoid #1"
-      , property (law_Difference_Eq_Monoid_1 @a))
+      , toProperty law_Difference_Eq_Monoid_1)
     , ( "Difference Eq Monoid #2"
-      , property (law_Difference_Eq_Monoid_2 @a))
+      , toProperty law_Difference_Eq_Monoid_2)
     , ( "Difference Eq Monoid #3"
-      , property (law_Difference_Eq_Monoid_3 @a))
+      , toProperty law_Difference_Eq_Monoid_3)
     ]
+  where
+    toProperty :: (a -> Bool) -> Property
+    toProperty fn = property
+        $ \a -> checkCoverage
+        $ cover 1  (a == mempty) "a == mempty"
+        $ cover 50 (a /= mempty) "a /= mempty"
+        $ fn a
 
 laws_Difference_PartialOrd
     :: forall a. (Arbitrary a, Show a, Difference a, PartialOrd a)
@@ -165,10 +179,17 @@ laws_Difference_PartialOrd
     -> Laws
 laws_Difference_PartialOrd _ = Laws "Difference PartialOrd"
     [ ( "Difference PartialOrd #1"
-      , property (law_Difference_PartialOrd_1 @a))
+      , toProperty law_Difference_PartialOrd_1)
     , ( "Difference PartialOrd #2"
-      , property (law_Difference_PartialOrd_2 @a))
+      , toProperty law_Difference_PartialOrd_2)
     ]
+  where
+    toProperty :: (a -> a -> Bool) -> Property
+    toProperty fn = property
+        $ \a1 a2 -> checkCoverage
+        $ cover 5 (a1 `geq` a2) "a1 `geq` a2"
+        $ cover 5 (a2 `geq` a1) "a2 `geq` a1"
+        $ fn a1 a2
 
 laws_Difference_PartialOrd_Semigroup
     :: forall a. (Arbitrary a, Show a, Difference a, PartialOrd a, Semigroup a)
@@ -176,10 +197,17 @@ laws_Difference_PartialOrd_Semigroup
     -> Laws
 laws_Difference_PartialOrd_Semigroup _ = Laws "Difference PartialOrd Semigroup"
     [ ( "Difference PartialOrd Semigroup #1"
-      , property (law_Difference_PartialOrd_Semigroup_1 @a))
+      , toProperty law_Difference_PartialOrd_Semigroup_1)
     , ( "Difference PartialOrd Semigroup #2"
-      , property (law_Difference_PartialOrd_Semigroup_2 @a))
+      , toProperty law_Difference_PartialOrd_Semigroup_2)
     ]
+  where
+    toProperty :: (a -> a -> Bool) -> Property
+    toProperty fn = property
+        $ \a1 a2 -> checkCoverage
+        $ cover 5 (a1 `geq` a2) "a1 `geq` a2"
+        $ cover 5 (a2 `geq` a1) "a2 `geq` a1"
+        $ fn a1 a2
 
 laws_Difference_PartialOrd_Monoid
     :: forall a. (Arbitrary a, Show a, Difference a, PartialOrd a, Monoid a)
@@ -187,8 +215,15 @@ laws_Difference_PartialOrd_Monoid
     -> Laws
 laws_Difference_PartialOrd_Monoid _ = Laws "Difference PartialOrd Monoid"
     [ ( "Difference PartialOrd Monoid #1"
-      , property (law_Difference_PartialOrd_Monoid_1 @a))
+      , toProperty law_Difference_PartialOrd_Monoid_1)
     ]
+  where
+    toProperty :: (a -> a -> Bool) -> Property
+    toProperty fn = property
+        $ \a1 a2 -> checkCoverage
+        $ cover 0.1 (a1 `geq` a2 && a2 /= mempty) "a1 `geq` a2 && a2 /= mempty"
+        $ cover 0.1 (a2 `geq` a1 && a1 /= mempty) "a2 `geq` a1 && a1 /= mempty"
+        $ fn a1 a2
 
 laws_Difference_Ord
     :: forall a. (Arbitrary a, Show a, Difference a, Ord a)
@@ -196,10 +231,17 @@ laws_Difference_Ord
     -> Laws
 laws_Difference_Ord _ = Laws "Difference Ord"
     [ ( "Difference Ord #1"
-      , property (law_Difference_Ord_1 @a))
+      , toProperty law_Difference_Ord_1)
     , ( "Difference Ord #2"
-      , property (law_Difference_Ord_2 @a))
+      , toProperty law_Difference_Ord_2)
     ]
+  where
+    toProperty :: (a -> a -> Bool) -> Property
+    toProperty fn = property
+        $ \a1 a2 -> checkCoverage
+        $ cover 5 (a1 >= a2) "a1 >= a2"
+        $ cover 5 (a2 >= a1) "a2 >= a1"
+        $ fn a1 a2
 
 laws_Difference_Ord_Semigroup
     :: forall a. (Arbitrary a, Show a, Difference a, Ord a, Semigroup a)
@@ -207,10 +249,17 @@ laws_Difference_Ord_Semigroup
     -> Laws
 laws_Difference_Ord_Semigroup _ = Laws "Difference Ord Semigroup"
     [ ( "Difference Ord Semigroup #1"
-      , property (law_Difference_Ord_Semigroup_1 @a))
+      , toProperty law_Difference_Ord_Semigroup_1)
     , ( "Difference Ord Semigroup #2"
-      , property (law_Difference_Ord_Semigroup_2 @a))
+      , toProperty law_Difference_Ord_Semigroup_2)
     ]
+  where
+    toProperty :: (a -> a -> Bool) -> Property
+    toProperty fn = property
+        $ \a1 a2 -> checkCoverage
+        $ cover 5 (a1 >= a2) "a1 >= a2"
+        $ cover 5 (a2 >= a1) "a2 >= a1"
+        $ fn a1 a2
 
 laws_Difference_Ord_Monoid
     :: forall a. (Arbitrary a, Show a, Difference a, Ord a, Monoid a)
@@ -218,8 +267,15 @@ laws_Difference_Ord_Monoid
     -> Laws
 laws_Difference_Ord_Monoid _ = Laws "Difference Ord Monoid"
     [ ( "Difference Ord Monoid #1"
-      , property (law_Difference_Ord_Monoid_1 @a))
+      , toProperty law_Difference_Ord_Monoid_1)
     ]
+  where
+    toProperty :: (a -> a -> Bool) -> Property
+    toProperty fn = property
+        $ \a1 a2 -> checkCoverage
+        $ cover 0.1 (a1 >= a2 && a2 /= mempty) "a1 >= a2 && a2 /= mempty"
+        $ cover 0.1 (a2 >= a1 && a1 /= mempty) "a2 >= a1 && a1 /= mempty"
+        $ fn a1 a2
 
 --------------------------------------------------------------------------------
 -- Instances

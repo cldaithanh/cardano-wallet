c2fc4cd04a96a0f364e6053a54b7811ec32adf52
Johannes Lund <johannes.lund@iohk.io>
2022-05-04 16:59:22 +0200

    Add dummyEraHistory

--- lib/core/src/Cardano/Wallet/Primitive/Slotting.hs
+++ lib/core/src/Cardano/Wallet/Primitive/Slotting.hs
@@ -64,6 +64,9 @@ module Cardano.Wallet.Primitive.Slotting
     , snapshot
     , hoistTimeInterpreter
     , expectAndThrowFailures
+
+     -- * Cardano API
+    , dummyEraHistory
     ) where
 
 import Prelude
@@ -74,6 +77,10 @@ import Cardano.BM.Data.Tracer
     ( HasSeverityAnnotation (..) )
 import Cardano.Slotting.EpochInfo.API
     ( EpochInfo )
+import Cardano.Slotting.Slot
+    ( EpochSize (EpochSize) )
+import Cardano.Slotting.Time
+    ( mkSlotLength )
 import Cardano.Wallet.Orphans
     ()
 import Cardano.Wallet.Primitive.Types
@@ -113,6 +120,8 @@ import Data.Text.Class
     ( ToText (..) )
 import Data.Time.Clock
     ( NominalDiffTime, UTCTime, addUTCTime, getCurrentTime )
+import Data.Time.Clock.POSIX
+    ( posixSecondsToUTCTime )
 import Data.Word
     ( Word32, Word64 )
 import Fmt
@@ -121,6 +130,13 @@ import GHC.Stack
     ( CallStack, HasCallStack, getCallStack, prettySrcLoc )
 import Ouroboros.Consensus.BlockchainTime.WallClock.Types
     ( RelativeTime (..), SystemStart (SystemStart), addRelTime )
+import Ouroboros.Consensus.HardFork.History
+    ( Bound (..)
+    , EraEnd (..)
+    , EraParams (..)
+    , EraSummary (..)
+    , SafeZone (StandardSafeZone)
+    )
 import Ouroboros.Consensus.HardFork.History.EpochInfo
     ( interpreterToEpochInfo )
 import Ouroboros.Consensus.HardFork.History.Qry
@@ -135,10 +151,13 @@ import Ouroboros.Consensus.HardFork.History.Qry
     , wallclockToSlot
     )
 import Ouroboros.Consensus.HardFork.History.Summary
-    ( neverForksSummary )
+    ( Summary (..), neverForksSummary )
+import Ouroboros.Consensus.Util.Counting
+    ( NonEmpty (NonEmptyCons, NonEmptyOne) )
 import UnliftIO.Exception
     ( throwIO )
 
+import qualified Cardano.Api as Cardano
 import qualified Cardano.Slotting.Slot as Cardano
 import qualified Data.Text as T
 import qualified Ouroboros.Consensus.BlockchainTime.WallClock.Types as Cardano
@@ -385,6 +404,8 @@ currentEpoch ti = do
 -- with a system start time as context.
 data TimeInterpreter m = forall eras. TimeInterpreter
     { interpreter :: m (Interpreter eras)
+     -- ^ NOTE:  interpreter ti cannot throw PastHorizonException, but
+     -- this way we don't have to carry around yet another type parameter.
     , blockchainStartTime :: StartTime
     , tracer :: Tracer m TimeInterpreterLog
     , handleResult :: forall a. Either PastHorizonException a -> m a
@@ -611,3 +632,55 @@ unsafeExtendSafeZone = f . neverFails r
         , handleResult = h
         }
     r = "unsafeExtendSafeZone should make PastHorizonExceptions impossible."
+
+
+{-------------------------------------------------------------------------------
+                        Cardano API EraHistory
+-------------------------------------------------------------------------------}
+
+-- | For testing only.
+dummyEraHistory :: (Cardano.EraHistory Cardano.CardanoMode, SystemStart)
+dummyEraHistory =
+    ( Cardano.EraHistory Cardano.CardanoMode int
+    , SystemStart $ posixSecondsToUTCTime 0
+    )
+  where
+    int = mkInterpreter summary
+
+    eraParams = EraParams
+            { eraEpochSize = EpochSize 432000
+            , eraSlotLength = mkSlotLength 1
+            , eraSafeZone = StandardSafeZone 129600
+            }
+
+    emptyEra = EraSummary
+            { eraStart = Bound
+                { boundTime = RelativeTime 0
+                , boundSlot = 0
+                , boundEpoch = Cardano.EpochNo 0
+                }
+            , eraEnd = EraEnd Bound
+                { boundTime = RelativeTime 0
+                , boundSlot = 0
+                , boundEpoch = Cardano.EpochNo 0
+                }
+            , eraParams = eraParams
+            }
+
+    summary =
+        Summary
+            { getSummary =
+                NonEmptyCons emptyEra -- Byron
+                $ NonEmptyCons emptyEra -- Shelley
+                $ NonEmptyCons emptyEra -- Allegra
+                $ NonEmptyCons emptyEra -- Mary
+                $ NonEmptyOne EraSummary -- Alonzo
+                    { eraStart = Bound
+                        { boundTime = RelativeTime 0
+                        , boundSlot = 0
+                        , boundEpoch = Cardano.EpochNo 0
+                        }
+                    , eraEnd = EraUnbounded
+                    , eraParams = eraParams
+                    }
+            }

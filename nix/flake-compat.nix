1b2c8676928271e31cf5f833021abefd781e2568
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2022-05-06 20:11:47 +1200

    Use pkgs.fetchzip when possible

--- nix/flake-compat.nix
+++ nix/flake-compat.nix
@@ -4,7 +4,7 @@ let
   lock = builtins.fromJSON (builtins.readFile (src + "/flake.lock"));
   flake-compate-input = lock.nodes.root.inputs.flake-compat;
   nixpkgs-input = lock.nodes.haskellNix.inputs.${builtins.elemAt lock.nodes.root.inputs.nixpkgs 1};
-  flake-compat = import (builtins.fetchTarball {
+  flake-compat = import (pkgs.fetchzip {
     url = "https://api.github.com/repos/input-output-hk/flake-compat/tarball/${lock.nodes.${flake-compate-input}.locked.rev}";
     sha256 = lock.nodes.${flake-compate-input}.locked.narHash;
   });

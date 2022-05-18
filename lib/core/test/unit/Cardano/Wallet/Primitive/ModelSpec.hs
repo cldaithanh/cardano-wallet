584e798f4cfa7ca264b1f0680a29088b7a2dbd20
Jonathan Knowles <jonathan.knowles@iohk.io>
2022-05-18 06:42:31 +0000

    Adjust `genPendingTxs` to allow generation of txs with invalid scripts.

--- lib/core/test/unit/Cardano/Wallet/Primitive/ModelSpec.hs
+++ lib/core/test/unit/Cardano/Wallet/Primitive/ModelSpec.hs
@@ -97,7 +97,14 @@ import Cardano.Wallet.Primitive.Types.Tx
     , txScriptInvalid
     )
 import Cardano.Wallet.Primitive.Types.Tx.Gen
-    ( genTx, genTxIn, genTxOut, shrinkTx, shrinkTxIn, shrinkTxOut )
+    ( genTx
+    , genTxIn
+    , genTxOut
+    , genTxScriptValidity
+    , shrinkTx
+    , shrinkTxIn
+    , shrinkTxOut
+    )
 import Cardano.Wallet.Primitive.Types.UTxO
     ( UTxO (..), balance, dom, excluding, filterByAddress, restrictedTo )
 import Cardano.Wallet.Primitive.Types.UTxO.Gen
@@ -165,6 +172,7 @@ import Test.QuickCheck
     , frequency
     , genericShrink
     , label
+    , liftArbitrary
     , listOf
     , oneof
     , property
@@ -1155,6 +1163,7 @@ instance Arbitrary (WithPending WalletState) where
                 --   change output and an explicit withdrawal in the
                 --   transaction.
                 let tokens = TokenBundle.fromCoin $ simulateFee $ txOutCoin out
+                scriptValidity <- liftArbitrary genTxScriptValidity
                 let pending = withWithdrawal $ Tx
                         { txId = arbitraryHash
                         , fee = Nothing
@@ -1166,7 +1175,7 @@ instance Arbitrary (WithPending WalletState) where
                         , outputs = [out {tokens}]
                         , withdrawals = mempty
                         , metadata = Nothing
-                        , scriptValidity = Nothing
+                        , scriptValidity
                         }
 
                 elements [Set.singleton pending, Set.empty]

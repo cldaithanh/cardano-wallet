daaf6ad131a204299087e7fd7b40bdee978a0dc4
Jonathan Knowles <jonathan.knowles@iohk.io>
2022-06-03 00:57:23 +0000

    Shorten type parameters.

--- lib/core/src/Cardano/Wallet/Primitive/Types/StateDeltaSeq.hs
+++ lib/core/src/Cardano/Wallet/Primitive/Types/StateDeltaSeq.hs
@@ -48,74 +48,70 @@ data StateDeltaSeq state delta = StateDeltaSeq
     }
     deriving (Eq, Show)
 
-headState :: StateDeltaSeq state delta -> state
+headState :: StateDeltaSeq s d -> s
 headState StateDeltaSeq {head} = head
 
-lastState :: StateDeltaSeq state delta -> state
+lastState :: StateDeltaSeq s d -> s
 lastState StateDeltaSeq {head, tail}
     | null tail = head
     | otherwise = snd (V.last tail)
 
-size :: StateDeltaSeq state delta -> Int
+size :: StateDeltaSeq s d -> Int
 size = length . tail
 
-fromState :: state -> StateDeltaSeq state delta
+fromState :: s -> StateDeltaSeq s d
 fromState state = StateDeltaSeq state V.empty
 
-toDeltaList :: StateDeltaSeq state delta -> [delta]
+toDeltaList :: StateDeltaSeq s d -> [d]
 toDeltaList = fmap fst . F.toList . tail
 
-toStateList :: StateDeltaSeq state delta -> NonEmpty state
+toStateList :: StateDeltaSeq s d -> NonEmpty s
 toStateList StateDeltaSeq {head, tail} =
     head :| (snd <$> F.toList tail)
 
 append
     :: Functor m
-    => (state -> delta -> m state)
-    -> StateDeltaSeq state delta
-    -> delta
-    -> m (StateDeltaSeq state delta)
+    => (s -> d -> m s)
+    -> StateDeltaSeq s d
+    -> d
+    -> m (StateDeltaSeq s d)
 append nextState seq@StateDeltaSeq {head, tail} delta =
     nextState (lastState seq) delta <&> \state -> StateDeltaSeq
         {head, tail = tail `V.snoc` (delta, state)}
 
 appendMany
     :: (Foldable f, Monad m)
-    => (state -> delta -> m state)
-    -> StateDeltaSeq state delta
-    -> f delta
-    -> m (StateDeltaSeq state delta)
+    => (s -> d -> m s)
+    -> StateDeltaSeq s d
+    -> f d
+    -> m (StateDeltaSeq s d)
 appendMany = foldM . append
 
 iterate
-    :: (StateDeltaSeq state delta -> Maybe (StateDeltaSeq state delta))
-    -> StateDeltaSeq state delta
-    -> NonEmpty (StateDeltaSeq state delta)
+    :: (StateDeltaSeq s d -> Maybe (StateDeltaSeq s d))
+    -> StateDeltaSeq s d
+    -> NonEmpty (StateDeltaSeq s d)
 iterate transform seq0 =
     loop (seq0 :| []) seq0
   where
     loop !acc !seq = maybe acc (\p -> loop (p `NE.cons` acc) p) (transform seq)
 
-dropHead
-    :: StateDeltaSeq state delta
-    -> Maybe (StateDeltaSeq state delta)
+dropHead :: StateDeltaSeq s d -> Maybe (StateDeltaSeq s d)
 dropHead StateDeltaSeq {tail}
     | null tail = Nothing
     | otherwise = Just StateDeltaSeq
         {head = snd $ V.head tail, tail = V.dropHead 1 tail}
 
-dropLast
-    :: StateDeltaSeq state delta
-    -> Maybe (StateDeltaSeq state delta)
+dropLast :: StateDeltaSeq s d -> Maybe (StateDeltaSeq s d)
 dropLast StateDeltaSeq {head, tail}
     | null tail = Nothing
     | otherwise = Just StateDeltaSeq
         {head, tail = V.dropLast 1 tail}
 
-dropHeads :: StateDeltaSeq state delta -> NonEmpty (StateDeltaSeq state delta)
+dropHeads :: StateDeltaSeq s d -> NonEmpty (StateDeltaSeq s d)
 dropHeads = iterate dropHead
 
-dropLasts :: StateDeltaSeq state delta -> NonEmpty (StateDeltaSeq state delta)
+dropLasts :: StateDeltaSeq s d -> NonEmpty (StateDeltaSeq s d)
 dropLasts = iterate dropLast
 
 -- Performs the following transformation:
@@ -123,10 +119,7 @@ dropLasts = iterate dropLast
 --    state_0 : delta_0_1 : state_1 : delta_1_2 : state_2 : ...
 --    state_0 : delta_0_1 +           delta_1_2 : state_2 : ...
 --
-mergeHead
-    :: (delta -> delta -> delta)
-    -> StateDeltaSeq state delta
-    -> Maybe (StateDeltaSeq state delta)
+mergeHead :: (d -> d -> d) -> StateDeltaSeq s d -> Maybe (StateDeltaSeq s d)
 mergeHead merge StateDeltaSeq {head, tail}
     | length tail < 2 = Nothing
     | otherwise = Just StateDeltaSeq
@@ -140,12 +133,9 @@ mergeHead merge StateDeltaSeq {head, tail}
 -- Performs the following transformation:
 --
 --    ... : state_2 : delta_2_1 : state_1 : delta_1_0 : state_0
---    ... : state_2 : delta_2-1 +         : delta_1_0 : state_0
+--    ... : state_2 : delta_2_1 +         : delta_1_0 : state_0
 --
-mergeLast
-    :: (delta -> delta -> delta)
-    -> StateDeltaSeq state delta
-    -> Maybe (StateDeltaSeq state delta)
+mergeLast :: (d -> d -> d) -> StateDeltaSeq s d -> Maybe (StateDeltaSeq s d)
 mergeLast merge StateDeltaSeq {head, tail}
     | length tail < 2 = Nothing
     | otherwise = Just StateDeltaSeq
@@ -156,32 +146,18 @@ mergeLast merge StateDeltaSeq {head, tail}
     (d_1_0,  s_0) = tail ! (length tail - 1)
     (d_2_1, _s_1) = tail ! (length tail - 2)
 
-mergeHeads
-    :: (delta -> delta -> delta)
-    -> StateDeltaSeq state delta
-    -> NonEmpty (StateDeltaSeq state delta)
+mergeHeads :: (d -> d -> d) -> StateDeltaSeq s d -> NonEmpty (StateDeltaSeq s d)
 mergeHeads = iterate . mergeHead
 
-mergeLasts
-    :: (delta -> delta -> delta)
-    -> StateDeltaSeq state delta
-    -> NonEmpty (StateDeltaSeq state delta)
+mergeLasts :: (d -> d -> d) -> StateDeltaSeq s d -> NonEmpty (StateDeltaSeq s d)
 mergeLasts = iterate . mergeLast
 
-isPrefixOf
-    :: (Eq state, Eq delta)
-    => StateDeltaSeq state delta
-    -> StateDeltaSeq state delta
-    -> Bool
+isPrefixOf :: (Eq s, Eq d) => StateDeltaSeq s d -> StateDeltaSeq s d -> Bool
 isPrefixOf s1 s2 = (&&)
     (toDeltaList s1  `L.isPrefixOf` toDeltaList s2)
     (NE.toList (toStateList s1) `L.isPrefixOf` NE.toList (toStateList s2))
 
-isSuffixOf
-    :: (Eq state, Eq delta)
-    => StateDeltaSeq state delta
-    -> StateDeltaSeq state delta
-    -> Bool
+isSuffixOf :: (Eq s, Eq d) => StateDeltaSeq s d -> StateDeltaSeq s d -> Bool
 isSuffixOf s1 s2 = (&&)
     (toDeltaList s1  `L.isSuffixOf` toDeltaList s2)
     (NE.toList (toStateList s1) `L.isSuffixOf` NE.toList (toStateList s2))

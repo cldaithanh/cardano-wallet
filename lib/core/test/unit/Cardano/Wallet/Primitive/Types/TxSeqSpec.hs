d6ec72d1c4325572504f99d4197e1ca0b0c8ceaf
Jonathan Knowles <jonathan.knowles@iohk.io>
2022-06-04 06:03:00 +0000

    Add more properties to `TxSeqSpec`.

--- lib/core/test/unit/Cardano/Wallet/Primitive/Types/TxSeqSpec.hs
+++ lib/core/test/unit/Cardano/Wallet/Primitive/Types/TxSeqSpec.hs
@@ -26,6 +26,24 @@ spec = do
         it "prop_genTxSeq_isValid" $
             prop_genTxSeq_isValid & property
 
+    describe "dropHeadTxs " $ do
+        it "prop_dropHeadTxs_isValid" $
+            prop_dropHeadTxs_isValid & property
+
+    describe "dropLastTxs " $ do
+        it "prop_dropLastTxs_isValid" $
+            prop_dropLastTxs_isValid & property
+
 prop_genTxSeq_isValid :: Property
 prop_genTxSeq_isValid =
     forAll (genTxSeq genUTxO genAddress) TxSeq.isValid
+
+prop_dropHeadTxs_isValid :: Property
+prop_dropHeadTxs_isValid =
+    forAll (genTxSeq genUTxO genAddress) $ \txs ->
+        all TxSeq.isValid (TxSeq.dropHeadTxs txs)
+
+prop_dropLastTxs_isValid :: Property
+prop_dropLastTxs_isValid =
+    forAll (genTxSeq genUTxO genAddress) $ \txs ->
+        all TxSeq.isValid (TxSeq.dropLastTxs txs)

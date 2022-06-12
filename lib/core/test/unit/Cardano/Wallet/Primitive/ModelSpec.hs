e3883f7eec078acbfb9cb4ad9e7bd9428daca2b2
Jonathan Knowles <jonathan.knowles@iohk.io>
2022-06-12 01:15:41 +0000

    Simpler.

--- lib/core/test/unit/Cardano/Wallet/Primitive/ModelSpec.hs
+++ lib/core/test/unit/Cardano/Wallet/Primitive/ModelSpec.hs
@@ -2399,13 +2399,13 @@ prop_applyBlocks_lastUTxO_allOurs :: BlockSeq -> Property
 prop_applyBlocks_lastUTxO_allOurs blockSeq =
     applyBlockSeqLastUTxO AllOurs blockSeq
         (blockSeqToHeadUTxO blockSeq)
-    ===
-        (blockSeqToLastUTxO blockSeq)
+    === (blockSeqToLastUTxO blockSeq)
 
 prop_applyBlocks_lastUTxO_noneOurs :: BlockSeq -> Property
 prop_applyBlocks_lastUTxO_noneOurs blockSeq =
-    applyBlockSeqLastUTxO NoneOurs blockSeq UTxO.empty
-        === UTxO.empty
+    applyBlockSeqLastUTxO NoneOurs blockSeq
+        UTxO.empty
+    === UTxO.empty
 
 prop_applyBlocks_lastUTxO_someOurs
     :: forall s. (s ~ IsOursIf2 Address RewardAccount)
@@ -2415,8 +2415,7 @@ prop_applyBlocks_lastUTxO_someOurs
 prop_applyBlocks_lastUTxO_someOurs blockSeq someOurs =
     applyBlockSeqLastUTxO someOurs blockSeq
         (UTxO.filterByAddress isOurAddress $ blockSeqToHeadUTxO blockSeq)
-    ===
-        (UTxO.filterByAddress isOurAddress $ blockSeqToLastUTxO blockSeq)
+    === (UTxO.filterByAddress isOurAddress $ blockSeqToLastUTxO blockSeq)
   where
     isOurAddress :: Address -> Bool
     isOurAddress = conditionA someOurs

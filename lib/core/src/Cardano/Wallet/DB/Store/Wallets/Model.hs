531178fc5b4a7560851f11e853c9b1f221dec174
Paolo Veronelli <paolo.veronelli@iohk.io>
2022-06-29 20:11:33 +0200

    `TransactionInfo` is computed from wallets-transactions store

--- lib/core/src/Cardano/Wallet/DB/Store/Wallets/Model.hs
+++ lib/core/src/Cardano/Wallet/DB/Store/Wallets/Model.hs
@@ -1,4 +1,8 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
 
@@ -14,8 +18,18 @@ module Cardano.Wallet.DB.Store.Wallets.Model where
 
 import Prelude
 
+import Cardano.Wallet.DB.Sqlite.Schema
+    ( TxCollateral (..)
+    , TxCollateralOut (..)
+    , TxCollateralOutToken (..)
+    , TxIn (..)
+    , TxMeta (..)
+    , TxOut (..)
+    , TxOutToken (..)
+    , TxWithdrawal (..)
+    )
 import Cardano.Wallet.DB.Sqlite.Types
-    ( TxId )
+    ( TxId (getTxId) )
 import Cardano.Wallet.DB.Store.Meta.Model
     ( DeltaTxMetaHistory (..)
     , ManipulateTxMetaHistory
@@ -23,31 +37,53 @@ import Cardano.Wallet.DB.Store.Meta.Model
     , mkTxMetaHistory
     )
 import Cardano.Wallet.DB.Store.Transactions.Model
-    ( TxHistory, TxHistoryF (..), mkTxHistory )
+    ( Decoration (With)
+    , TxHistory
+    , TxHistory
+    , TxHistoryF (..)
+    , TxRelationF (..)
+    , WithTxOut (..)
+    , mkTxHistory
+    )
+import Cardano.Wallet.Primitive.Slotting
+    ( TimeInterpreter, interpretQuery, slotToUTCTime )
+import Cardano.Wallet.Primitive.Types.TokenMap
+    ( AssetId (AssetId) )
+import Cardano.Wallet.Primitive.Types.Tx
+    ( TxMeta (..), TxOut (..) )
 import Data.Delta
     ( Delta (..) )
 import Data.DeltaMap
     ( DeltaMap (Adjust, Insert) )
+import Data.Foldable
+    ( toList )
 import Data.Function
     ( (&) )
+import Data.Functor
+    ( (<&>) )
+import Data.Generics.Internal.VL
+    ( (^.) )
 import Data.Map.Strict
     ( Map )
+import Data.Quantity
+    ( Quantity (..) )
 import Data.Set
     ( Set )
 import Fmt
     ( Buildable, build )
 
+import qualified Cardano.Wallet.DB.Sqlite.Schema as DB
 import qualified Cardano.Wallet.DB.Store.Meta.Model as TxMetaStore
 import qualified Cardano.Wallet.DB.Store.Transactions.Model as TxStore
 import qualified Cardano.Wallet.Primitive.Types as W
-import qualified Cardano.Wallet.Primitive.Types.Tx as W
-import Data.Foldable
-    ( toList )
+import qualified Cardano.Wallet.Primitive.Types.Coin as WC
+import qualified Cardano.Wallet.Primitive.Types.TokenBundle as TokenBundle
+import qualified Cardano.Wallet.Primitive.Types.Tx as WT
 import qualified Data.Map.Strict as Map
 import qualified Data.Set as Set
 
 data DeltaTxWalletsHistory
-    = ExpandTxWalletsHistory W.WalletId [(W.Tx, W.TxMeta)]
+    = ExpandTxWalletsHistory W.WalletId [(WT.Tx, WT.TxMeta)]
     | ChangeTxMetaWalletsHistory W.WalletId ManipulateTxMetaHistory
     | GarbageCollectTxWalletsHistory
     | RemoveWallet W.WalletId
@@ -85,3 +121,79 @@ linkedTransactions
 
 walletsLinkedTransactions :: Map W.WalletId TxMetaHistory -> Set TxId
 walletsLinkedTransactions = Set.unions . toList .  fmap linkedTransactions
+
+mkTransactionInfo :: Monad m
+    => TimeInterpreter m
+    -> W.BlockHeader
+    -> TxRelationF 'With
+    -> DB.TxMeta
+    -> m WT.TransactionInfo
+mkTransactionInfo ti tip TxRelationF {..} DB.TxMeta {..} = do
+    txTime <- interpretQuery ti . slotToUTCTime $ txMetaSlot
+    return
+        $ WT.TransactionInfo
+        { WT.txInfoId = getTxId txMetaTxId
+        , WT.txInfoFee = WC.Coin . fromIntegral <$> txMetaFee
+        , WT.txInfoInputs = mkTxIn <$> ins
+        , WT.txInfoCollateralInputs = map mkTxCollateral collateralIns
+        , WT.txInfoOutputs = mkTxOut <$> outs
+        , WT.txInfoCollateralOutput = fmap mkTxCollateralOut collateralOuts
+        , WT.txInfoWithdrawals = Map.fromList $ map mkTxWithdrawal withdrawals
+        , WT.txInfoMeta = WT.TxMeta
+              { WT.status = txMetaStatus
+              , WT.direction = txMetaDirection
+              , WT.slotNo = txMetaSlot
+              , WT.blockHeight = Quantity (txMetaBlockHeight)
+              , amount = txMetaAmount
+              , WT.expiry = txMetaSlotExpires
+              }
+        , WT.txInfoMetadata = txMetadata
+        , WT.txInfoDepth = Quantity
+              $ fromIntegral
+              $ if tipH > txMetaBlockHeight
+                  then tipH - txMetaBlockHeight
+                  else 0
+        , WT.txInfoTime = txTime
+        , WT.txInfoScriptValidity = txMetaScriptValidity <&> \case
+              False -> WT.TxScriptInvalid
+              True -> WT.TxScriptValid
+        }
+  where
+    tipH = getQuantity $ tip ^. #blockHeight
+    mkTxIn (WithTxOut tx out) =
+        ( WT.TxIn
+          { WT.inputId = getTxId (txInputSourceTxId tx)
+          , WT.inputIx = txInputSourceIndex tx
+          }
+        , txInputSourceAmount tx
+        , mkTxOut <$> out)
+    mkTxCollateral (WithTxOut tx out) =
+        ( WT.TxIn
+          { WT.inputId = getTxId (txCollateralSourceTxId tx)
+          , WT.inputIx = txCollateralSourceIndex tx
+          }
+        , txCollateralSourceAmount tx
+        , mkTxOut <$> out)
+    mkTxOut (out,tokens) =
+        WT.TxOut
+        { address = txOutputAddress out
+        , WT.tokens = TokenBundle.fromFlatList
+              (txOutputAmount out)
+              (mkTxOutToken <$> tokens)
+        }
+    mkTxOutToken token =
+        ( AssetId (txOutTokenPolicyId token) (txOutTokenName token)
+        , txOutTokenQuantity token)
+    mkTxCollateralOut (out,tokens) =
+        WT.TxOut
+        { address = txCollateralOutAddress out
+        , WT.tokens = TokenBundle.fromFlatList
+              (txCollateralOutAmount out)
+              (mkTxCollateralOutToken <$> tokens)
+        }
+    mkTxCollateralOutToken token =
+        ( AssetId
+              (txCollateralOutTokenPolicyId token)
+              (txCollateralOutTokenName token)
+        , txCollateralOutTokenQuantity token)
+    mkTxWithdrawal w = (txWithdrawalAccount w, txWithdrawalAmount w)
